19bca8a2efd2d8c154fedf58379abc9e576ed569
b14 <124437680+b14uob3@users.noreply.github.com>
2023-02-27 21:48:19 +0100

    fixes

--- programs/four-in-a-row/src/instructions/add_checker.rs
+++ programs/four-in-a-row/src/instructions/add_checker.rs
@@ -9,10 +9,11 @@ pub struct AddChecker<'info> {
 }
 
 impl<'info> AddChecker<'info> {
+    #[access_control(Self::constraints(&self, col))]
     pub fn process(&mut self, col: u8) -> Result<()> {
         let Self { signer, board, .. } = self;
 
-        if signer.key() == board.red.unwrap() || signer.key() == board.yellow.unwrap() {
+        if signer.key() == board.red || signer.key() == board.yellow {
             board.add_checker(col as usize);
             msg!("added checker");
         }
@@ -22,9 +23,9 @@ impl<'info> AddChecker<'info> {
 
     pub fn constraints(&self, col: u8) -> Result<()> {
         require!(self.board.turn().is_some(), ErrorCode::Unauthorized);
+        // require!(self.board.turn_key() == self.signer.key(), ErrorCode::Unauthorized);
         require!(self.board.board[0][col as usize].is_none(), ErrorCode::ColumnFull);
         require!(col < COLS as u8, ErrorCode::InvalidColumn);
-
 
         Ok(())
     }
