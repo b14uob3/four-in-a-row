803958332eb0ab679f426e85bf03694cbb13b3ee
b14 <124437680+b14uob3@users.noreply.github.com>
2023-03-13 12:01:42 +0100

    upgrade to react 18

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,10 +1,11 @@
+import { createRoot } from 'react-dom/client';
 import React, { StrictMode } from 'react';
-import ReactDOM from 'react-dom';
 import { App } from './App';
 
-ReactDOM.render(
+const container = document.getElementById('app');
+const root = createRoot(container!);
+root.render(
     <StrictMode>
         <App />
-    </StrictMode>,
-    document.getElementById('app')
+    </StrictMode>
 );
